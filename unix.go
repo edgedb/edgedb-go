divan/gel-go

add TLS support

unix.go
@@ -18,4 +18,10 @@
 
 package edgedb
 
+import "crypto/x509"
+
 var defaultHosts = []string{"/run/edgedb", "/var/run/edgedb"}
+
+func getCertPool() (*x509.CertPool, error) {
+	return x509.SystemCertPool()
+}

edgedb/edgedb-go

unix.go
@@ -18,4 +18,10 @@
 
 package edgedb
 
+import "crypto/x509"
+
 var defaultHosts = []string{"/run/edgedb", "/var/run/edgedb"}
+
+func getSystemCertPool() (*x509.CertPool, error) {
+	return x509.SystemCertPool()
+}