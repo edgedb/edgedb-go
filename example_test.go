93e91f00e59813dc072e7f59c189ab8d126679b4
fmoor <fmoor@gmx.com>
2022-08-22 12:43:38 -0700

    Add link property to struct mapping example

---
+++ example_test.go
@@ -0,0 +1,67 @@
+// This source file is part of the EdgeDB open source project.
+//
+// Copyright 2020-present EdgeDB Inc. and the EdgeDB authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package edgedb_test
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	"github.com/edgedb/edgedb-go"
+)
+
+var (
+	ctx    = context.Background()
+	client = createClient()
+)
+
+func createClient() *edgedb.Client {
+	c, err := edgedb.CreateClient(ctx, edgedb.Options{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return c
+}
+
+// [Link properties] are treated as fields in the linked to struct, and the @
+// is omitted from the field's tag.
+//
+// [Link properties]: https://www.edgedb.com/docs/guides/link_properties
+func Example_linkProperty() {
+	var result []struct {
+		Name    string `edgedb:"name"`
+		Friends []struct {
+			Name     string                 `edgedb:"name"`
+			Strength edgedb.OptionalFloat64 `edgedb:"strength"`
+		} `edgedb:"friends"`
+	}
+
+	_ = client.Query(
+		ctx,
+		`select Person {
+		name,
+		friends: {
+			name,
+			@strength,
+		}
+	}`,
+		&result,
+	)
+}
+
+// TestNil makes this not a whole file example. // https://go.dev/blog/examples
+func TestNil(t *testing.T) {}
