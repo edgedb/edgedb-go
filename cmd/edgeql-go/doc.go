50c1ca50514ceb6639a6ae56a831581b65f8d8f8
fmoor <fmoor@gmx.com>
2022-08-24 12:01:03 -0700

    Update documentation

--- cmd/edgeql-go/doc.go
+++ cmd/edgeql-go/doc.go
@@ -14,30 +14,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// edgeql-go is a tool to generate go functions from edgeql queries. Given the
-// name of one or more edgeql files edgeql-go will create a new self-contained
-// Go source with a function for each edgeql query.
+// edgeql-go is a tool to generate go functions from edgeql queries. When run
+// in an EdgeDB project directory (or subdirectory) a *_edgeql.go source file
+// will be generated for each *.edgeql file.  The generated go will have an
+// EdgeqlFileName and EdgeqlFileNameJSON function with typed arguments and
+// return value matching the query's arguments and result shape.
 //
 // # Install
 //
 //	go install github.com/edgedb/edgedb-go/cmd/edgeql-go@latest
 //
-// See this link for [pinning tool dependencies].
+// See also [pinning tool dependencies].
 //
 // # Usage
 //
-// Typically this process would be run using go generate, like this:
+// Typically this process would be run using [go generate] like this:
 //
-//	//go:generate edgeql-go -file=myquery.edgeql
-//
-// The -file flag may be specified multiple times to include more than one
-// query in the output file.
-//
-// The -out flag is the path to the go source file to be created. If omitted,
-// the file is created next to the first edgeql file provided.
-//
-// The -json flag tells edgeql-go to return json from the query instead of go
-// structs.
+//	//go:generate edgeql-go
 //
 // [pinning tool dependencies]: https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+// [go generate]: https://go.dev/blog/generate
 package main
